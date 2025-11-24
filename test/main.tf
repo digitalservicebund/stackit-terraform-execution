14e359a6ff08654f4f79bf71f3f207dd1418daaf
Volker Hartmann <volker.hartmann@digitalservice.bund.de>
2025-11-24 09:39:55 +0100

    check test values

--- test/main.tf
+++ test/main.tf
@@ -7,8 +7,18 @@ terraform {
   }
 }
 
-resource "null_resource" "test" {
+resource "null_resource" "service_account_key" {
   provisioner "local-exec" {
-    command = "echo $STACKIT_SERVICE_ACCOUNT_KEY_PATH"
+    command = "cat $STACKIT_SERVICE_ACCOUNT_KEY_PATH"
+  }
+}
+resource "null_resource" "backend_key_id" {
+  provisioner "local-exec" {
+    command = "echo $AWS_ACCESS_KEY_ID"
+  }
+}
+resource "null_resource" "backend_secret_key" {
+  provisioner "local-exec" {
+    command = "echo $AWS_SECRET_ACCESS_KEY"
   }
 }
