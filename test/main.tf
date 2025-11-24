7d5d7de9f785b7c985d21511080c4a39b7190095
Volker Hartmann <volker.hartmann@digitalservice.bund.de>
2025-11-24 09:37:48 +0100

    fix terraform code

--- test/main.tf
+++ test/main.tf
@@ -1,5 +1,14 @@
-null_resource "example" "example" {
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.4"
+    }
+  }
+}
+
+resource "null_resource" "test" {
   provisioner "local-exec" {
-    command = "echo Hello, World!"
+    command = "echo $STACKIT_SERVICE_ACCOUNT_KEY_PATH"
   }
 }
